4a00dc8ff49af255a9086e31ed723f2db6f29681
Dilshad Abdulla <dilshad.a73@gmail.com>
2024-04-29 00:56:27 +0100

    create blogs and main templates

--- djseries/blogs/views.py
+++ djseries/blogs/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+	template_name = "index.html"
+	context = {}
+	return render(request, template_name, context)
+
+
+def post_list_view(request):
+	template_name = "posts.html"
+	context = {}
+	return render(request, template_name, context)
+
+
+def post_detail_view(request):
+	template_name = "detail.html"
+	context = {}
+	return render(request, template_name, context)
+
+
+def post_edit_view(request):
+	template_name = "edit.html"
+	context = {}
+	return render(request, template_name, context)
+
+
+def post_create_view(request):
+	template_name = "create.html"
+	context = {}
+	return render(request, template_name, context)
+
+
+def post_delete_view(request):
+	template_name = "delete.html"
+	context = {}
+	return render(request, template_name, context)
